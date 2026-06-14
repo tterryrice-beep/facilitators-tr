d0bd90cdbf572a082e308baa2f34561bef0dd55e
Макс Бормот <bormotmax@MacBook-Pro-Maks.local>
2026-06-14 23:09:22 +0300

    init

---
+++ _ReadMe.md
@@ -0,0 +1,4 @@
+
+# Links
+[Spotlight Template](https://tailwindcss.com/plus/templates/spotlight/preview)
+[Tailwind Docs](https://tailwindcss.com/docs)
---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite'
+import react, { reactCompilerPreset } from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+import babel from '@rolldown/plugin-babel'
+
+// https://vite.dev/config/
+export default defineConfig({
+  server: {
+    host: "0.0.0.0", 
+    open: true
+  },
+  plugins: [
+    react(),
+    babel({ presets: [reactCompilerPreset()] }),
+    tailwindcss(),
+  ],
+})
