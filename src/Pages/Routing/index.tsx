4dabcb63423d93944a93b9ec7cc8b99b4a5cf529
= <=>
2026-06-23 19:48:13 +0300

    expand Routing page content with problem statement and PathRouter rationale

--- src/Pages/Routing/index.tsx
+++ src/Pages/Routing/index.tsx
@@ -1,23 +1,25 @@
-import React, { type FC, useState } from "react";
+import React, { type FC } from "react";
 
 import { useTranslate } from "@/providers/LocaleProvider/hook";
-import css from "./style.module.scss";
-import { createRoute } from "@/modules/PathRouter/utils";
-import { route } from "@/config";
-import { Heading } from "@@/Heading";
-import { Text } from "@@/Text";
-import { ObjectView } from "@@/ObjectView";
 
-const { modals, pages } = createRoute(route);
+import { ObjectView } from "@@/ObjectView";
+import { Heading } from "@@/Heading";
+import { JSXView } from "@@/JSXView";
+import { Text } from "@@/Text";
+
+import css from "./style.module.scss";
+
+// const { modals, pages } = createRoute(route);
 
 const Page: FC = ({}) => {
   const { getText, changeLanguage } = useTranslate();
+
   return (
     <div className={css.page}>
       <Heading title={"Routing"} rightBar={<div>github ling be here</div>} />
 
-      <section id="overview">
-        <Text type="subtitle">Overview</Text>
+      <section id={css.overview} className="mt-6">
+        <Text type="subtitle">Проблематика</Text>
         <br />
         <br />
         <Text>
@@ -30,23 +32,103 @@ const Page: FC = ({}) => {
           <ObjectView
             defaultExpanded
             data={{
-              route: [
+              routes: [
                 {
                   path: "/about",
-                  component: "AboutRoot",
+                  element: "<AboutRoot />",
                 },
                 {
                   path: "/about/terms",
-                  component: "Terms",
+                  element: "<Terms />",
                 },
                 {
                   path: "/about/privacy",
-                  component: "Privacy",
+                  element: "<Privacy />",
                 },
               ],
             }}
           />
+
+          <JSXView>{`
+<Routes>
+  {routes.map((data) => <Route {...data} />)}
+</Routes>`}</JSXView>
         </div>
+
+        <Text>
+          Але зазвичай воно реалізовано як у типових Реакт-посібниках:
+        </Text>
+
+        <JSXView>{`
+<Routes>
+  <Route path="/about" element={<AboutRoot />} />
+  <Route path="/about/terms" element={<Terms />} />
+  <Route path="/about/privacy" element={<Privacy />} />
+  ...
+</Routes>`}</JSXView>
+        <br />
+        <br />
+
+        <Text>Які проблеми із цього, виникали особисто у мене:</Text>
+        <ul className="list-disc mt-4">
+          <li className="ml-8">
+            <Text>
+              Додавання нових сторінок, чи, прости Господи, переадресація.
+            </Text>
+          </li>
+
+          <li className="ml-8">
+            <Text>Робота із посиланнями та переходами між сторінками.</Text>
+            <br />
+            <Text>
+              Щастить, коли всі маршрути зберігаються у документації, чи хоча би
+              в енумі. Але на практиці, доводиться залазити у файл, та шукати,
+              потрібний шлях.
+            </Text>
+          </li>
+
+          <li className="ml-8">
+            <Text>Модалки.</Text>
+            <br />
+            <Text>
+              Є ситуації коли для модалок прописують окремий маршрут типу:
+            </Text>
+            <br />
+            <ObjectView
+              defaultExpanded
+              data={{
+                routes: [
+                  {
+                    path: "/about",
+                    element: "<About />",
+                  },
+                  {
+                    path: "/about/modal",
+                    element: "<About />",
+                  },
+                ],
+              }}
+            />
+            <br />
+            <Text>І це навіть не перебільшення</Text>
+          </li>
+        </ul>
+        <br />
+        <Text>
+          Тож, зрештою, я написав власне рішення, котре особисто мене би
+          влаштовувало
+        </Text>
+      </section>
+
+      <section id={css.about}>
+        <Text type="subtitle">Для чого взагалі існує PathRouter</Text>
+        <br />
+        <br />
+
+        <Text>
+              В багатьох проектах над якими я працював підозріло часто траплялися проблеми із типізацією маршрутів. 
+        </Text>
+
       </section>
     </div>
   );
